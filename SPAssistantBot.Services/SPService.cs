333c0302d87d96f80b83d5980bd38c1fd0a701be
Rajendra Bhide <Rajendra.bhide@outlook.com>
2020-10-01 19:55:32 +1300

    Code Tidy up

--- SPAssistantBot.Services/SPService.cs
+++ SPAssistantBot.Services/SPService.cs
@@ -10,23 +10,12 @@ namespace SPAssistantBot.Services
 {
     public class SPService : BaseO365Service
     {
-        //private readonly string aadApplicationId;
-        //private readonly string aadClientSecret;
-        //private readonly string spTenant;
-        //private readonly string keyVaultSecretIdentifier;
-        //private readonly Microsoft.Extensions.Logging.ILogger log;
-        //private readonly KeyVaultService keyVaultService;
+       
         private readonly SPCustomisationService _spCustomisationService;
+
         public SPService(IConfiguration configuration, KeyVaultService keyVaultService, SPCustomisationService spCustomisationService,
             ILogger<SPService> log) : base(configuration, keyVaultService, log)
         {
-            //this.aadApplicationId = configuration["AADClientId"];
-            //this.aadClientSecret = configuration["AADClientSecret"];
-            //this.spTenant = configuration["Tenant"];
-            //this.keyVaultSecretIdentifier = configuration["KeyVaultSecretIdentifier"];
-            //this.keyVaultService = keyVaultService;
-            ////this.certificate509 = certificate509;
-            //this.log = log;
             _spCustomisationService = spCustomisationService;
         }
 
@@ -66,31 +55,5 @@ namespace SPAssistantBot.Services
             return teamSiteUrl;
         }
 
-         
-
-        //private async Task<string> GetAccessToken(string[] scopes)
-        //{
-        //    using (var certificate509 = KVService.GetCertificateAsync())
-        //    {
-        //        IConfidentialClientApplication clientApp = ConfidentialClientApplicationBuilder.Create(AADApplicationId).WithCertificate(certificate509).WithTenantId(SPTenant).Build();
-        //        var authResult = await clientApp.AcquireTokenForClient(scopes).ExecuteAsync();
-        //        var accessToken = authResult.AccessToken;
-        //        return accessToken;
-        //    }
-        //}
-
-        //private ClientContext GetClientContextWithAccessToken(string url, string accessToken)
-        //{
-        //    Log.LogInformation($"Attempting to get SPO context for site {url}");
-
-        //    var context = new ClientContext(url);
-
-        //    context.ExecutingWebRequest += delegate (object sender, WebRequestEventArgs webRequestEventArgs)
-        //    {
-        //        webRequestEventArgs.WebRequestExecutor.RequestHeaders["Authorization"] = $"Bearer {accessToken}";
-        //    };
-
-        //    return context;
-        //}
     }
 }
